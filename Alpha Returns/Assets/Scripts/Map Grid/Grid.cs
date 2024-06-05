0ecc1ec414011dbcaab3fcd2f57d00c46d254060
Fareza Athala <farezaathala10@gmail.com>
2024-06-05 10:51:26 +0700

    add Grid Map

---
+++ Alpha Returns/Assets/Scripts/Map Grid/Grid.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEditor;
+
+public class Grid : MonoBehaviour {
+    public float waterLevel = .4f;
+    public float scale = .1f;
+    public int size = 100;
+
+    Cell[,] grid;
+
+    public void GenerateGrid()
+    {
+        float[,] noiseMap = GenerateNoiseMap();
+        float[,] fallOffMap = GenerateFallOffMap();
+
+        grid = new Cell[size, size];
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                float noiseValue = noiseMap[x, y];
+                noiseValue -= fallOffMap[x, y];
+                bool isWater = noiseValue < waterLevel;
+                Cell cell = new Cell(isWater);
+                grid[x, y] = cell;
+            }
+        }
+    }
+
+    private float[,] GenerateNoiseMap() {
+        float[,] noiseMap = new float[size, size];
+        (float xOffSet, float yOffSet) = (Random.Range(-10000f, 10000f), Random.Range(-10000f, 10000f));
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                float noiseValue = Mathf.PerlinNoise(x * scale + xOffSet, y * scale + yOffSet);
+                noiseMap[x, y] = noiseValue;
+            }
+        }
+        return noiseMap;
+    }
+
+    private float[,] GenerateFallOffMap() {
+        float[,] fallOffMap = new float[size, size];
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                float xv = x / (float)size * 2 - 1;
+                float yv = y / (float)size * 2 - 1;
+                float v = Mathf.Max(Mathf.Abs(xv), Mathf.Abs(yv));
+                fallOffMap[x, y] = Mathf.Pow(v, 3f) / (Mathf.Pow(v, 3f) + Mathf.Pow(2.2f - 2.2f * v, 3f));
+            }
+        }
+        return fallOffMap;
+    }
+
+    private void OnDrawGizmos() {
+        if (grid == null) return;
+
+        float offset = size / 2f;
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                Cell cell = grid[x, y];
+                Gizmos.color = cell.isWater ? Color.blue : Color.green;
+                Vector3 pos = new Vector3(x - offset, y - offset, 0);
+                Gizmos.DrawCube(pos, Vector3.one);
+            }
+        }
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(Grid))]
+public class GridEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Grid grid = (Grid)target;
+
+        if (GUILayout.Button("Generate Random Grid"))
+        {
+            grid.GenerateGrid();
+            EditorUtility.SetDirty(grid);
+        }
+    }
+}
+#endif
